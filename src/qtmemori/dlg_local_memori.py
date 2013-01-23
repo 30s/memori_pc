cce82b480d48a6ebe6aabf59ca688173c67f3fb6
Robert <rzheng@xtremeprog.com>
2013-01-23 14:29:48 +0800

    * integrate django & show memoris

--- src/qtmemori/dlg_local_memori.py
+++ src/qtmemori/dlg_local_memori.py
@@ -1,13 +1,27 @@
+import os
+import sys
+
 from PySide.QtCore import *
 from PySide.QtGui import *
 
 from ui_dlg_local_memori import Ui_DlgLocalMemori
+
+DIR_NAME = os.path.dirname(os.path.abspath(__file__))
+
+sys.path.append(DIR_NAME + '/../djmemori')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djmemori.settings")
+
+from djmemori.models import Photo
+from djmemori.views import event_generator
 
 
 class DlgLocalMemori(QDialog, Ui_DlgLocalMemori):
     def __init__(self, parent=None):
         super(DlgLocalMemori, self).__init__(parent)
         self.setupUi(self)
+        for i in event_generator():
+            item = QListWidgetItem(str(i[0].date_taken) + ' %d photos' % len(i))
+            self.lst_memori.addItem(item)
 
 
 if __name__=='__main__':
