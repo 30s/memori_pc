e653ac74fa40b12f0639214d8ec1b41c728c9680
Robert <rzheng@xtremeprog.com>
2013-01-25 14:19:23 +0800

    * add main window

---
+++ src/qtmemori/main_memori.py
@@ -0,0 +1,62 @@
+import os
+import sys
+
+import Image
+
+from PySide.QtCore import *
+from PySide.QtGui import *
+
+from ui_main_memori import Ui_MainWindow
+
+DIR_NAME = os.path.dirname(os.path.abspath(__file__))
+
+sys.path.append(DIR_NAME + '/../djmemori')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djmemori.settings")
+
+from djmemori.models import Photo
+from djmemori.views import event_generator
+from utils import get_square_thumb_80
+
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        self.setupUi(self)
+        self.events = [i for i in event_generator()]
+        for i in self.events:
+            item = QListWidgetItem(i[0].date_taken.strftime('%Y-%m-%d %H:%M:%S') + '\tphotos %d' % len(i))
+            self.lst_memori.addItem(item)
+        self.lst_memori.itemClicked.connect(self.on_lst_memori_item_clicked)
+        for i in range(10):
+            for j in range(10):
+                lbl = QLabel()
+                self.grid_photos.addWidget(lbl, i, j, 1, 1)
+
+    def on_lst_memori_item_clicked(self, item):
+        for i in range(10):
+            for j in range(10):
+                self.grid_photos.itemAtPosition(i, j).widget().setPixmap(None)
+        idx = 0
+        for i in self.events[self.lst_memori.indexFromItem(item).row()][::-1]:
+            if idx >= 100:
+                return
+            photo = i.root.path + i.path
+            thumb = get_square_thumb_80(i.root.path, i.path)
+            img = QPixmap(thumb)
+            lbl = self.grid_photos.itemAtPosition(idx / 10, idx % 10).widget()
+            lbl.setPixmap(img)
+            idx += 1
+
+    def on_lst_photos_item_clicked(self, item):
+        photo = self.events[self.lst_memori.currentRow()][self.lst_photos.indexFromItem(item).row()]
+        im = Image.open(photo.root.path + photo.path)
+        im.show()
+
+
+if __name__=='__main__':
+    import sys
+    app = QApplication(sys.argv)
+    main = MainWindow()
+    main.show()
+    sys.exit(app.exec_())
+    
---
+++ src/qtmemori/ui_main_memori.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'main_memori.ui'
+#
+# Created: Fri Jan 25 14:18:14 2013
+#      by: pyside-uic 0.2.13 running on PySide 1.1.0
+#
+# WARNING! All changes made in this file will be lost!
+
+from PySide import QtCore, QtGui
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtGui.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.horizontalLayout = QtGui.QHBoxLayout(self.centralwidget)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.verticalLayout = QtGui.QVBoxLayout()
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.lst_memori = QtGui.QListWidget(self.centralwidget)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.lst_memori.sizePolicy().hasHeightForWidth())
+        self.lst_memori.setSizePolicy(sizePolicy)
+        self.lst_memori.setMaximumSize(QtCore.QSize(300, 16777215))
+        self.lst_memori.setObjectName("lst_memori")
+        self.verticalLayout.addWidget(self.lst_memori)
+        self.btn_export = QtGui.QPushButton(self.centralwidget)
+        self.btn_export.setObjectName("btn_export")
+        self.verticalLayout.addWidget(self.btn_export)
+        self.horizontalLayout.addLayout(self.verticalLayout)
+        self.frame = QtGui.QFrame(self.centralwidget)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.frame.sizePolicy().hasHeightForWidth())
+        self.frame.setSizePolicy(sizePolicy)
+        self.frame.setFrameShape(QtGui.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtGui.QFrame.Raised)
+        self.frame.setObjectName("frame")
+        self.verticalLayout_2 = QtGui.QVBoxLayout(self.frame)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.grid_photos = QtGui.QGridLayout()
+        self.grid_photos.setObjectName("grid_photos")
+        self.verticalLayout_2.addLayout(self.grid_photos)
+        self.horizontalLayout.addWidget(self.frame)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtGui.QMenuBar()
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 22))
+        self.menubar.setObjectName("menubar")
+        self.menu_settings = QtGui.QMenu(self.menubar)
+        self.menu_settings.setObjectName("menu_settings")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtGui.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.menubar.addAction(self.menu_settings.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QtGui.QApplication.translate("MainWindow", "MainWindow", None, QtGui.QApplication.UnicodeUTF8))
+        self.btn_export.setText(QtGui.QApplication.translate("MainWindow", "Export", None, QtGui.QApplication.UnicodeUTF8))
+        self.menu_settings.setTitle(QtGui.QApplication.translate("MainWindow", "Settings", None, QtGui.QApplication.UnicodeUTF8))
+
