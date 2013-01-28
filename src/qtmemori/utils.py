87a670e9bdc1575402ba7008b719c8e6ea25e586
Robert <rzheng@xtremeprog.com>
2013-01-28 14:36:31 +0800

    * list android devices & import photos

--- src/qtmemori/utils.py
+++ src/qtmemori/utils.py
@@ -26,3 +26,36 @@ def get_square_thumb_80(root, path):
     thumb = thumb.resize((80, 80), Image.ANTIALIAS)
     thumb.convert('RGB').save(filename)
     return filename
+
+
+def list_android_devices():
+    devices = []
+    pip = os.popen('adb -d devices')
+    pip.readline() # escape first line
+    line = pip.readline()
+    while len(line) != 0:
+        segs = line.split()
+        if (len(segs) == 2) and (segs[1] == 'device'):
+            devices.append(segs[0])
+        line = pip.readline()
+    pip.close()
+    return devices
+
+
+def import_photos_from_android(name, path):
+    photos = []
+    pip = os.popen('adb -s %s shell ls /sdcard/DCIM/Camera/' % name)
+    line = pip.readline()
+    while len(line) != 0:
+        print line
+        if line.endswith('.jpg\r\n'):
+            os.system('adb -s %s pull /sdcard/DCIM/Camera/%s %s' % (name, line[:-2], path))
+            photos.append(line[:-2])
+        line = pip.readline()
+    return photos
+
+
+if __name__=='__main__':
+    print list_android_devices()
+    print import_photos_from_android('a2af63a9',
+                                     '/Users/teamx/workspace/memori_pc/src/qtmemori/photos')
